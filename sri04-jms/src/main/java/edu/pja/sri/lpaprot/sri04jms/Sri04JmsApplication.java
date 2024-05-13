fdfab984a5b6150de64bf49fcbff833b19d872e4
niepomaker <paprotlukasz@gmail.com>
2024-05-13 16:06:06 +0200

    inicjalizacja ćwiczenia

---
+++ sri04-jms/src/main/java/edu/pja/sri/lpaprot/sri04jms/Sri04JmsApplication.java
@@ -0,0 +1,13 @@
+package edu.pja.sri.lpaprot.sri04jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sri04JmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sri04JmsApplication.class, args);
+	}
+
+}
---
+++ sri04-jms/src/test/java/edu/pja/sri/lpaprot/sri04jms/Sri04JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pja.sri.lpaprot.sri04jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sri04JmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
